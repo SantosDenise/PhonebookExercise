b78ceee288f812275e1279f0ee293281ecc7e414
SantosDenise <131408127+SantosDenise@users.noreply.github.com>
2023-04-20 22:32:00 +0100

    Add files via upload
    uploaded initial files after working on files for first time. project running on local desktop, suitable to display efforts made and code on show

---
+++ PhoneDirectory/AddPhNo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory
+{
+    public class AddPhNo
+    {
+        public Dictionary<string, string> contacts = new Dictionary<string, string>();
+
+        public void WritingInFile(string FullName, string PhoneNo)
+        {
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\DBS\OneDrive - Dublin Business School (DBS)\Desktop\AlgorithmDataStructure_2023\PhoneDirectory\ContactList.txt", true))
+            {
+                writer.WriteLine($"{FullName},{PhoneNo}");
+            }
+
+        }
+        public void ReadingFromFile()
+        {
+            if (File.Exists(@"C:\Users\DBS\OneDrive - Dublin Business School (DBS)\Desktop\AlgorithmDataStructure_2023\PhoneDirectory\ContactList.txt"))
+            {
+                using (StreamReader reader = new StreamReader(@"C:\Users\DBS\OneDrive - Dublin Business School (DBS)\Desktop\AlgorithmDataStructure_2023\PhoneDirectory\ContactList.txt"))
+                {
+                    while (reader.Peek() >= 0)
+                    {
+                        string line = reader.ReadLine();
+                        string[] array = line.Split(',');
+                        string name = array[0];
+                        string phoneNum = array[1];
+                        contacts.Add(phoneNum, name);
+                    }
+                }
+
+            }
+        }
+
+        public void UpdateContactsFiles()
+        {
+            // get the local contact dict
+            string filePath = @"C:\Users\DBS\OneDrive - Dublin Business School (DBS)\Desktop\AlgorithmDataStructure_2023\PhoneDirectory\ContactList.txt";
+
+            //delete the file
+            if (File.Exists(filePath)) 
+            {
+                File.Delete(filePath);
+            }
+
+            //update the file
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                using (TextWriter tw = new StreamWriter(fs))
+
+                    foreach (KeyValuePair<string, string> kvp in contacts)
+                    {
+                        tw.WriteLine(string.Format("{1},{0}", kvp.Key, kvp.Value));
+                    }
+            }
+
+        }
+
+    }
+}
---
+++ PhoneDirectory/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Xml.Linq;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Media;
+
+namespace PhoneDirectory
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CollectionViewSource cs = new CollectionViewSource();
+        AddPhNo phoneNum = new AddPhNo();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddPhNo_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtFullName.Text;
+            string phoneNo = txtPhoneNo.Text;
+
+            phoneNum.contacts.Add(phoneNo, name);
+            phoneNum.WritingInFile(name, phoneNo);
+            txtFullName.Clear();
+            txtPhoneNo.Clear();
+
+        }
+
+        private void btnLookup_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Phone Number");
+            string name = txtFullName.Text;
+            string phoneNo = txtPhoneNo.Text;
+            if (phoneNo != "")
+            {
+                phoneNum.contacts.TryGetValue(phoneNo, out name);
+                dgPhoneDirectory.DataContext = false;
+                dt.Rows.Add(name, phoneNo);
+                cs.Source = dt.DefaultView;
+                dgPhoneDirectory.ItemsSource = cs.View;
+            }
+            else
+            {
+                foreach (var contact in phoneNum.contacts)
+                {
+                    if (contact.Value.ToLower() == name.ToLower())
+                    {
+                        string key = contact.Key;
+                        dgPhoneDirectory.DataContext = false;
+                        dt.Rows.Add(name, key);
+                        cs.Source = dt.DefaultView;
+                        dgPhoneDirectory.ItemsSource = cs.View;
+                    }
+                }
+            }
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtFullName.Text;
+            string phoneNo = txtPhoneNo.Text;
+            string newNumber = txtPhoneNo_Copy.Text;
+            string temp = "";
+
+            //todo if key already exists throw error back to screen and tell user to pick a different
+            //number its already in use
+
+            bool v = phoneNum.contacts.TryGetValue(phoneNo, out temp);
+            if (v)
+            {
+                //if we find the key remove the current one and add the new one
+                phoneNum.contacts.Remove(phoneNo);
+                phoneNum.contacts.Add(newNumber,name);
+            }
+
+            // now call btnview click to update the tabel again
+            ViewPhoneDirectory();
+            btnView_Click(sender, e);
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtFullName.Text;
+            string phoneNo = txtPhoneNo.Text;
+            string newNumber = txtPhoneNo_Copy.Text;
+            if(name=="" || phoneNo == "" || newNumber == "")
+            {
+                // Break;
+            }
+
+            phoneNum.UpdateContactsFiles();
+
+            phoneNum.contacts.Remove(phoneNo);
+            phoneNum.contacts.Add(name, phoneNo);
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            phoneNum.ReadingFromFile();
+        }
+        public void ViewPhoneDirectory()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Phone Number");
+
+
+            if (phoneNum.contacts.Count == 0)
+            {
+                Console.WriteLine("You dont have contacts on your list");
+            }
+            else
+            {
+                foreach(var c in phoneNum.contacts)
+                {
+                    dt.Rows.Add(c.Value, c.Key);
+                }
+            }
+
+            cs.Source = dt.DefaultView;
+            dgPhoneDirectory.ItemsSource = cs.View;
+
+        }
+
+        private void btnView_Click(object sender, RoutedEventArgs e)
+        {
+            ViewPhoneDirectory();
+        }
+    }
+}
